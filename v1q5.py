3fa07a9ab2321985fe7eedb5aa2b001ba5c1e745
z5098773 <z5098773@cse.unsw.EDU.AU>
2016-11-08 22:16:26 +1100

    comments on golf

--- v1q5.py
+++ v1q5.py
@@ -2,3 +2,15 @@
 import re, sys, fileinput
 lines = [ line.rstrip() for line in sys.stdin ]
 print('\n'.join(map(lambda x: re.sub(r'^#(\d+)$', lambda y: lines[int(y.group(1))], x), lines)))
+#read all lines from stdin
+#replaces all of the lines that start with # followed by numbers by the content of that line number
+#example
+#input:
+#hello
+##0
+#world
+
+#output:
+#hello
+#hello
+#world

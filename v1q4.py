c471abb32f1465450792a24d48adbb254034cbb4
z5098773 <z5098773@cse.unsw.EDU.AU>
2016-11-08 21:35:51 +1100

    comments

--- v1q4.py
+++ v1q4.py
@@ -1,3 +1,4 @@
 #!/usr/local/bin/python3.5
-import sys, re
+import sys, re;
 [ print(re.sub(r"(\d+(\.\d+)+)", lambda x: str(int(round(float(x.group(0))))) if re.match(r'^(\d+(\.\d+){0,1})$', x.group(0)) else x.group(0), line.rstrip())) for line in sys.stdin ]
+#for all lines in standard in, replaces floats with its rounded value
